5f01484c81ddb78cf50b9f2b5583c2bafd57f01a
NemethNorbert <norbert.nemeth@liferay.com>
2018-11-15 13:10:56 +0100

    Solves metal.js module load

--- modules/apps/bookmarks/bookmarks-web/src/main/resources/META-INF/resources/js/HelloWorld.es.js
+++ modules/apps/bookmarks/bookmarks-web/src/main/resources/META-INF/resources/js/HelloWorld.es.js
@@ -1,27 +1,9 @@
-import 'metal';
-import 'metal-component';
-import PortletBase from 'frontend-js-web/liferay/PortletBase.es';
+import Component from 'metal-component';
 import Soy from 'metal-soy';
-import {Config} from 'metal-state';
 
 import templates from './HelloWorld.soy';
 
-/**
- * HelloWorld
- *
- * This component shows a list of available folders to move content in and
- * allows to filter them by searching.
- */
-
-class HelloWorld extends PortletBase {
-
-
-}
-
-HelloWorld.STATE = {
-
-
-};
+class HelloWorld extends Component {}
 
 Soy.register(HelloWorld, templates);
 
