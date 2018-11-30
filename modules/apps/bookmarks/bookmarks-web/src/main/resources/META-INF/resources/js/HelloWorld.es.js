2e8df7b42a0e7082b18d30db54a3d95c610dba96
Roland Pákai <roland.pakai@liferay.com>
2018-11-30 19:33:18 +0100

    LPS-87276 workiing on the accordion

--- modules/apps/bookmarks/bookmarks-web/src/main/resources/META-INF/resources/js/HelloWorld.es.js
+++ modules/apps/bookmarks/bookmarks-web/src/main/resources/META-INF/resources/js/HelloWorld.es.js
@@ -3,14 +3,34 @@ import Soy from 'metal-soy';
 import {Config} from 'metal-state';
 
 
-
-
-import Footer from './Footer.soy';
-import Header from './Header.soy';
-
 import templates from './HelloWorld.soy';
 
-class HelloWorld extends Component {}
+class HelloWorld extends Component {
+	
+	
+	render() {
+		
+		
+		//if collapsible
+		var storeTask = AUI().debounce(Liferay.Store, 100);
+
+		$('#supportedContentTypes').on(
+			'hide.bs.collapse show.bs.collapse',
+			function(event) {
+				if (event.target.id === 'supportedContentTypes') {
+					storeTask(
+						{
+							'supportedContentTypes': (event.type === 'hide')
+						}
+					);
+				}
+			}
+		);
+		///
+
+	}
+	
+}
 
 HelloWorld.STATE = {
 	releaseInfo: Config.string()
