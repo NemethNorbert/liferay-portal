ebe80cefe6601a81063b57fcaf83a4ca77627f8f
Norbert Nemeth <norbert.nemeth@liferay.com>
2019-11-12 11:28:36 +0100

    LPS-104414 nullcheck on originalField

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-web/src/main/resources/META-INF/resources/js/ddm_form.js
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-web/src/main/resources/META-INF/resources/js/ddm_form.js
@@ -4105,9 +4105,16 @@ AUI.add(
 					var newFieldLocalizations = repeatedField.get(
 						'localizationMap'
 					);
-					var totalLocalizations = originalField.get(
-						'localizationMap'
-					);
+
+					var totalLocalizations;
+
+					if (originalField) {
+						totalLocalizations = originalField.get(
+							'localizationMap'
+						);
+					} else {
+						totalLocalizations = {};
+					}
 
 					var currentLocale = repeatedField.get('displayLocale');
 
@@ -4139,7 +4146,13 @@ AUI.add(
 					repeatedField.set('localizationMap', newFieldLocalizations);
 
 					var newNestedFields = repeatedField.get('fields');
-					var originalNestedFields = originalField.get('fields');
+					var originalNestedFields;
+
+					if (originalField) {
+						originalNestedFields = originalField.get('fields');
+					} else {
+						originalNestedFields = [];
+					}
 
 					for (var i = 0; i < newNestedFields.length; i++) {
 						instance.populateBlankLocalizationMap(
