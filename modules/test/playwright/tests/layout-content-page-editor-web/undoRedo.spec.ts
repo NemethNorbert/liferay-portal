cfb725020e9b3bbb4af21c3c8b441c9f03d03190
NemethNorbert <norbert.nemeth@liferay.com>
2024-01-25 16:31:19 +0100

    LPD-8428 Add test for History button functionality

--- modules/test/playwright/tests/layout-content-page-editor-web/undoRedo.spec.ts
+++ modules/test/playwright/tests/layout-content-page-editor-web/undoRedo.spec.ts
@@ -156,3 +156,109 @@ test('Undo and Redo buttons work as expected', async ({
 
 	await apiHelpers.headlessSite.deleteSite(site.id);
 });
+
+test('Undo history works as expected', async ({
+	apiHelpers,
+	page,
+	pageEditorPage,
+}) => {
+	await page.goto('/');
+
+	// Create a site
+
+	const site = await apiHelpers.headlessSite.createSite(getRandomId());
+
+	// Create a page with a Heading fragment
+
+	const headingId = getRandomId();
+
+	const headingFragment = getFragmentDefinition(
+		headingId,
+		'BASIC_COMPONENT-heading'
+	);
+
+	const layout = await apiHelpers.headlessDelivery.createSitePage(
+		site.id,
+		getRandomId(),
+		getPageDefinition([headingFragment])
+	);
+
+	// Go to edit mode of page
+
+	await pageEditorPage.goToEditMode(site, layout);
+
+	// Assert History button is visible
+
+	await expect(page.getByTitle('History')).toBeVisible();
+
+	// Select the fragment
+
+	await pageEditorPage.selectFragment(headingId);
+
+	// Go to General Panel and change the Heading level 3 times
+
+	await pageEditorPage.goToConfigurationTab('General');
+	await expect(page.getByLabel('Heading Level', {exact: true})).toBeVisible();
+	await page.getByLabel('Heading Level', {exact: true}).selectOption('h2');
+	await expect(
+		page
+			.locator(`.lfr-layout-structure-item-${headingId}`)
+			.locator('h2.component-heading')
+	).toBeAttached();
+	await page.getByLabel('Heading Level', {exact: true}).selectOption('h3');
+	await expect(
+		page
+			.locator(`.lfr-layout-structure-item-${headingId}`)
+			.locator('h3.component-heading')
+	).toBeAttached();
+	await page.getByLabel('Heading Level', {exact: true}).selectOption('h4');
+	await expect(
+		page
+			.locator(`.lfr-layout-structure-item-${headingId}`)
+			.locator('h4.component-heading')
+	).toBeAttached();
+
+	// Open the History dropdown and assert we have 3 + 1 Action inlcuding Undo All
+
+	await page.getByTitle('History').click();
+	await expect(
+		page.locator('#clay-dropdown-menu-2').locator('ul > li > button')
+	).toHaveCount(4);
+
+	// Assert the current History position is disabled
+
+	await expect(
+		page
+			.getByRole('menuitem', {name: 'Update Heading Configuration'})
+			.first()
+	).toBeDisabled();
+
+	// Assert the heading fragment has the correct heading level after changing history
+
+	await page
+		.getByRole('menuitem', {name: 'Update Heading Configuration'})
+		.nth(1)
+		.click();
+	await expect(
+		page
+			.locator(`.lfr-layout-structure-item-${headingId}`)
+			.locator('h3.component-heading')
+	).toBeAttached();
+
+	// Assert the new History position is updated
+
+	await expect(
+		page
+			.getByRole('menuitem', {name: 'Update Heading Configuration'})
+			.first()
+	).toBeEnabled();
+	await expect(
+		page
+			.getByRole('menuitem', {name: 'Update Heading Configuration'})
+			.nth(1)
+	).toBeDisabled();
+
+	// Delete the site
+
+	await apiHelpers.headlessSite.deleteSite(site.id);
+});
