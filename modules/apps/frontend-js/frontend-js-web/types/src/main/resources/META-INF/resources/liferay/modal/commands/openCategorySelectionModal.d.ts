2777df720d87a02c89d9f6cff6831e02297cdaa5
NemethNorbert <norbert.nemeth@liferay.com>
2024-04-04 16:02:50 +0200

    LPD-6814 Typescript types

--- modules/apps/frontend-js/frontend-js-web/types/src/main/resources/META-INF/resources/liferay/modal/commands/openCategorySelectionModal.d.ts
+++ modules/apps/frontend-js/frontend-js-web/types/src/main/resources/META-INF/resources/liferay/modal/commands/openCategorySelectionModal.d.ts
@@ -3,14 +3,27 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
+interface Category {
+	categoryId: string;
+	className: string;
+	classNameId: string;
+	classPK: string;
+	nodePath: string;
+	title: string;
+	value: string;
+	vocabularyId: number;
+}
 export default function openCategorySelectionModal({
+	onSelectMethod,
 	portletNamespace,
 	redirectURL,
 	selectCategoryURL,
 	title,
 }: {
+	onSelectMethod: (selectedItems: Record<string, Category>) => {} | null;
 	portletNamespace: string;
 	redirectURL: string;
 	selectCategoryURL: string;
 	title: string;
 }): void;
+export {};
