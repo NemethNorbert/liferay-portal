48d1ec849ea11f09e4049e8c90d310232b5ac1d1
Jorge González <jorge.gonzalez@liferay.com>
2024-11-14 16:50:08 +0100

    LPD-42067 Add and apply playwright project config

--- modules/test/playwright/playwright.config.ts
+++ modules/test/playwright/playwright.config.ts
@@ -14,6 +14,7 @@ import {config as analyticsSettingsWebConfig} from './tests/analytics-settings-w
 import {config as analyticsWebConfig} from './tests/analytics-web/config';
 import {config as announcementsWebConfig} from './tests/announcements-web/config';
 import {config as assetPublisherWebConfig} from './tests/asset-publisher-web/config';
+import {config as assetTagsAdminWebConfig} from './tests/asset-tags-admin-web/config';
 import {config as batchPlannerConfig} from './tests/batch-planner/config';
 import {config as blogsWebConfig} from './tests/blogs-web/config';
 import {config as calendarWebConfig} from './tests/calendar-web/config';
@@ -129,6 +130,7 @@ export default defineConfig({
 		depotWebConfig,
 		announcementsWebConfig,
 		assetPublisherWebConfig,
+		assetTagsAdminWebConfig,
 		batchPlannerConfig,
 		blogsWebConfig,
 		calendarWebConfig,
